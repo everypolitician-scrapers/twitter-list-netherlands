78bde90df23a0c7d93dab3185eea5029a6388950
Chris Mytton <chrismytton@gmail.com>
2017-08-24 18:51:45 +0100

    Remove list that no longer exists

--- scraper.rb
+++ scraper.rb
@@ -11,7 +11,6 @@ abort "Need to set ENV['MORPH_TWITTER_TOKENS']" unless ENV.key? 'MORPH_TWITTER_T
 twitter_list = TwitterList::Scraper.new(twitter_tokens: ENV['MORPH_TWITTER_TOKENS'])
 
 list_array = twitter_list.people('Simon_Tol', 'tweede-kamer') +
-              twitter_list.people('Simon_Tol', 'kabinet-rutte-ii') +
               twitter_list.people('PolitwoopsNL', 'tweede-kamer-overig')
 
 ScraperWiki.save_sqlite([:id], list_array.uniq)
